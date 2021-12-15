c8121b54906647d20bbd71a97ad1dd9b2b791610
Wellinton Costa <wcostaee@gmail.com>
2021-12-14 23:11:03 -0300

    Fixing exceptions and improving extract_item to accept non-exact matches

--- monthly_report.py
+++ monthly_report.py
@@ -1,16 +1,19 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
+import re
+
+from scrap_exceptions import PageLoadingFailed, PageContentInvalid, ItemNotFound
 
 class Monthly_Report:
     def __init__(self, base_url: str):
         self.__base_url = base_url
     
     def __extract_item(self, item: str, from_body=False) -> str:
-        result = self.__raw_data.find(string=item)
+        result = self.__raw_data.find(text=re.compile(item))
         if result:
             return result.parent.parent.next_sibling.string
-        raise ValueError("Item not found")
+        raise ItemNotFound(f"Item not found: {item}")
 
     def __format_item(self, item: str, format: str):
         if format == "float":
@@ -20,12 +23,12 @@ class Monthly_Report:
         return datetime.strptime(item, "%m/%Y")
 
     def __extract_all_data(self):
-        valuation = self.__extract_item("Patrimônio Líquido – R$ ")
-        amount_quotes = self.__extract_item("Número de Cotas Emitidas ")
-        shareholder_quantity = self.__extract_item("Número de cotistas ")
-        reference_date = self.__extract_item("Competência: ")
+        valuation = self.__extract_item("Patrimônio Líquido")
+        amount_quotes = self.__extract_item("Número de Cotas Emitidas")
+        shareholder_quantity = self.__extract_item("Número de cotistas")
+        reference_date = self.__extract_item("Competência:")
         output = {
-            "name": self.__extract_item("Código ISIN: "),
+            "isin_name": self.__extract_item("Código ISIN:"),
             "valuation": self.__format_item(valuation, "float"),
             "amount_quotes": self.__format_item(amount_quotes, "int"),
             "shareholder_quantity": self.__format_item(shareholder_quantity, "int"),
@@ -44,13 +47,12 @@ class Monthly_Report:
         headers = {"accept": "text/html"}
         response = requests.get(url, headers=headers)
         if response.status_code != 200:
-            raise ValueError("This page is invalid")
+            raise PageLoadingFailed(f"Page loading failed. Status code: {response.status_code}")
         self.__raw_data = BeautifulSoup(response.text, "html.parser")
         if self.__is_valid():
-            print(self.__extract_all_data())
-            return None
-        raise ValueError("This page content is invalid")
+            return self.__extract_all_data()
+        raise PageContentInvalid("This page does not seem to be valid")
 
 # HTTP because otherwise it would be necessary to deal with certificates
 report = Monthly_Report("http://fnet.bmfbovespa.com.br")
-report.get_report_from_document_id(237235)
+print(report.get_report_from_document_id(237235))
