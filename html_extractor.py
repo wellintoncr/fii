0a46533e88a3a9f54d171913aa4893a6a84b0d31
Wellinton Costa <wcostaee@gmail.com>
2021-12-15 22:32:39 -0300

    It is not necessary to provide base_url anymore once it is always the same

--- html_extractor.py
+++ html_extractor.py
@@ -1,10 +1,16 @@
-import requests
+# Project related
 from scrap_exceptions import PageLoadingFailed
+
+# External libraries
+import requests
 
 class HTML_Extractor:
     @staticmethod
-    def get_raw_from_document_id(base_url: str, document_id: int):
-        """Get page HTML based on 'document_id'."""
+    def get_raw_from_document_id(document_id: int):
+        """Get page HTML based on 'document_id'.
+        If the webserver returns something different from 200, raise PageLoadingFailed
+        """
+        base_url = "http://fnet.bmfbovespa.com.br"
         url = f"{base_url}/fnet/publico/exibirDocumento?id={document_id}"
         headers = {"accept": "text/html"}
         response = requests.get(url, headers=headers, timeout=3)
