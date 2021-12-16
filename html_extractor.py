f6ccbf4fa26f537917b113bf7bf41f25dd7473fc
Wellinton Costa <wcostaee@gmail.com>
2021-12-15 22:55:23 -0300

    Timeout can be passed to HTML_Extractor method

--- html_extractor.py
+++ html_extractor.py
@@ -6,14 +6,15 @@ import requests
 
 class HTML_Extractor:
     @staticmethod
-    def get_raw_from_document_id(document_id: int):
+    def get_raw_from_document_id(document_id: int, timeout=None):
         """Get page HTML based on 'document_id'.
-        If the webserver returns something different from 200, raise PageLoadingFailed
+        If the webserver returns something different from 200, raise PageLoadingFailed.
+        'timeout', in seconds, define how long the request can wait until is cancelled.
         """
         base_url = "http://fnet.bmfbovespa.com.br"
         url = f"{base_url}/fnet/publico/exibirDocumento?id={document_id}"
         headers = {"accept": "text/html"}
-        response = requests.get(url, headers=headers, timeout=3)
+        response = requests.get(url, headers=headers, timeout=timeout)
         if response.status_code != 200:
             raise PageLoadingFailed(f"Invalid status: {response.status_code}")
         return response.text
