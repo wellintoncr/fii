dffc3a8f75221b957b57c6e5cd32bfbc863fac09
Wellinton Costa <wcostaee@gmail.com>
2022-09-16 19:16:52 -0300

    Improving script to make use of new extraction flow

--- web_scraper/script.py
+++ web_scraper/script.py
@@ -1,21 +1,26 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-# from dividend_report import DividendReport
-from monthly_report import MonthlyReport
+from html_extractor import HTMLExtractor
 import scrap_exceptions as exc
+from scraper import Scraper
 
 first_document_id = 257065
-quantity_documents = 101
+quantity_documents = 500
 
 with ThreadPoolExecutor() as executor:
-    monthly_report = MonthlyReport()
     future_document = {
-        executor.submit(monthly_report.get_report_from_document_id, document_id): document_id
+        executor.submit(HTMLExtractor.get_raw_from_document_id, document_id): document_id
         for document_id in range(first_document_id, first_document_id + quantity_documents)
     }
     for future in as_completed(future_document):
         document_id = future_document[future]
         try:
-            print(future.result())
-        except (exc.PageContentError, exc.PageLoadingError) as err:
-            print(f"Failed ({document_id}): {err}")
+            html_raw = future.result()
+            scraper = Scraper(html_raw)
+            report = scraper.extract_report()
+            if report["report_type"]:
+                print(report)
+        except (exc.PageContentError, exc.PageLoadingError):
+            pass
+        except exc.ItemNotFoundError as err:
+            print(err, document_id)
