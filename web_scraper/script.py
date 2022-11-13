06b1f8c353f81cd3941813f6241f019e168c9bf7
Wellinton Costa <wcostaee@gmail.com>
2022-11-13 11:56:21 -0300

    Changing algorithm to autodetect the first report ID and keep incrementing it based on batch_size

--- web_scraper/script.py
+++ web_scraper/script.py
@@ -3,17 +3,25 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from html_extractor import HTMLExtractor
 
 import orjson
+import os
 
 import scrap_exceptions as exc
 from scraper import Scraper
 
-first_document_id = 200000
-last_document_id = 354000
+batch_size = 1000
+
+def first_report_id():
+    raw_files_list = os.listdir("output/")
+    reports_id = [int(each.split(".")[0]) for each in raw_files_list if ".json" in each]
+    reports_id.sort(reverse=True)
+    return reports_id[0] if reports_id else 200_000
+
 
 with ThreadPoolExecutor() as executor:
+    first_document_id = first_report_id()
     future_document = {
         executor.submit(HTMLExtractor.get_raw_from_document_id, document_id): document_id
-        for document_id in range(first_document_id, last_document_id + 1)
+        for document_id in range(first_document_id, first_document_id + batch_size + 1)
     }
     for future in as_completed(future_document):
         document_id = future_document[future]
