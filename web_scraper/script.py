dd81c10adfa93d176c1efc3ecbd39b1079e80346
Wellinton Costa <wcostaee@gmail.com>
2022-11-13 12:14:12 -0300

    Failure detection to improve range detection so it will be possible to know which ID is the first and last one to look for data

--- web_scraper/script.py
+++ web_scraper/script.py
@@ -1,14 +1,16 @@
+import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from html_extractor import HTMLExtractor
 
 import orjson
-import os
 
 import scrap_exceptions as exc
 from scraper import Scraper
 
 batch_size = 1000
+MAX_LONGEST_FAILURE_COUNTER = 1000
+
 
 def first_report_id():
     raw_files_list = os.listdir("output/")
@@ -17,25 +19,54 @@ def first_report_id():
     return reports_id[0] if reports_id else 200_000
 
 
-with ThreadPoolExecutor() as executor:
-    first_document_id = first_report_id()
-    future_document = {
-        executor.submit(HTMLExtractor.get_raw_from_document_id, document_id): document_id
-        for document_id in range(first_document_id, first_document_id + batch_size + 1)
-    }
-    for future in as_completed(future_document):
-        document_id = future_document[future]
-        if document_id % 100 == 0:
-            print(f"Processing {document_id}")
-        try:
-            html_raw = future.result()
-            scraper = Scraper(html_raw)
-            report = scraper.extract_report()
-            if report["report_type"]:
-                with open(f"output/{document_id}.json", "w+") as file:
-                    content = orjson.dumps(report).decode()
-                    file.write(content)
-        except exc.ItemNotFoundError as err:
-            print("Failed", err, document_id)
-        except Exception:
-            pass
+def get_longest_run():
+    raw_files_list = os.listdir("failures/")
+    reports_id = [int(each.split(".")[1]) for each in raw_files_list if "not_found" in each]
+    longest_run = 0
+    reports_id.sort(reverse=True)
+    reference = None
+    for position, report_id in enumerate(reports_id):
+        if position < len(reports_id) - 1:
+            if report_id == reports_id[position + 1] + 1:
+                longest_run += 1
+                if longest_run >= MAX_LONGEST_FAILURE_COUNTER:
+                    reference = report_id
+                    break
+            else:
+                longest_run = 0
+    return longest_run, reference
+
+
+longest_run, reference = get_longest_run()
+if longest_run == MAX_LONGEST_FAILURE_COUNTER:
+    print(f"""
+        Longest consecutive failure counter reached ({longest_run}) at {reference}.
+        You may increase this threshold or maybe there is no report beyond the last valid one
+    """)
+else:
+    with ThreadPoolExecutor() as executor:
+        first_document_id = first_report_id()
+        future_document = {
+            executor.submit(HTMLExtractor.get_raw_from_document_id, document_id): document_id
+            for document_id in range(first_document_id, first_document_id + batch_size + 1)
+        }
+        for future in as_completed(future_document):
+            document_id = future_document[future]
+            if document_id % 100 == 0:
+                print(f"Processing {document_id}")
+            try:
+                html_raw = future.result()
+                scraper = Scraper(html_raw)
+                report = scraper.extract_report()
+                if report["report_type"]:
+                    with open(f"output/{document_id}.json", "w+") as file:
+                        content = orjson.dumps(report).decode()
+                        file.write(content)
+                else:
+                    with open(f"failures/not_found.{document_id}.txt", "w+") as file:
+                        file.write("Nothing to extract")
+            except exc.ItemNotFoundError as err:
+                with open(f"failures/{document_id}.txt", "w+") as file:
+                    file.write(f"{err} || {document_id}")
+            except Exception:
+                pass
