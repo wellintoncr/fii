2fee9f79158f429895172d9e47c5e218d74f7446
Wellinton Costa <wcostaee@gmail.com>
2022-11-15 22:41:13 -0300

    New function to group all reports belonging to the same FII

--- analyzer/report_analyzer.py
+++ analyzer/report_analyzer.py
@@ -1,4 +1,7 @@
 import os
+from copy import deepcopy
+from datetime import datetime
+
 import orjson
 
 
@@ -16,4 +19,26 @@ def gather_all_reports(folder_name: str):
             to_append = content["data"]
             to_append["document_id"] = int(each.split(".")[0])
             output[report_type].append(to_append)
+    return output
+
+
+def group_all_reports(all_reports: dict):
+    output = {}
+    all_data = deepcopy(all_reports["monthly_report"])
+    all_data.extend(deepcopy(all_reports["dividend_report"]))
+    for each in all_data:
+        isin_name = each.pop("isin_name")
+        if isin_name not in output.keys():
+            output[isin_name] = {"name": None}
+        if each.get("name"):
+            output[isin_name]["name"] = each.pop("name")
+        date = each.get("payment_date", each.get("reference_date"))
+        date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
+        key = date.strftime("%m/%Y")
+        if key not in output[isin_name].keys():
+            output[isin_name][key] = {"dividend_report": [], "monthly_report": []}
+        if each.get("dividend"):
+            output[isin_name][key]["dividend_report"].append(each)
+        else:
+            output[isin_name][key]["monthly_report"].append(each)
     return output
--- tests/analyzer/test_report_analyzer.py
+++ tests/analyzer/test_report_analyzer.py
@@ -1,4 +1,5 @@
 import analyzer.report_analyzer as report_analyzer
+import tests.mocks.reports as reports_mock
 
 
 def test_gather_all_reports():
@@ -10,3 +11,8 @@ def test_gather_all_reports():
     assert len(result["dividend_report"]) == 1
     assert result["dividend_report"][0]["document_id"] == 333333
     assert result["dividend_report"][0]["isin_name"] == "ISIN_MOCK"
+
+
+def test_group_all_reports():
+    result = report_analyzer.group_all_reports(reports_mock.REPORTS_GATHERED_RAW)
+    assert result == reports_mock.REPORTS_GATHERED_EXPECTED
